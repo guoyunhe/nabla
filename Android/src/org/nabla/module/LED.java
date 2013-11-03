5fd6a72cc63e991d9f18534e6acf15eb8db2e30e
guoyunhebrave <guoyunhebrave@gmail.com>
2013-11-03 10:48:38 +0800

    Create LED.java

---
+++ Android/src/org/nabla/module/LED.java
@@ -0,0 +1,46 @@
+package org.nabla.module;
+
+import org.nabla.system.Hub;
+
+public class LED extends Element {
+	private boolean status;
+
+	public final static boolean ON = true;
+	public final static boolean OFF = false;
+
+	public LED() {
+
+	}
+
+	public LED(int ledid) {
+		id = ledid;
+	}
+
+	public LED(int ledid, Hub ledhub) {
+		id = ledid;
+		hub = ledhub;
+	}
+
+	void on() {
+		status = ON;
+		hub.output("LED" + " " + Integer.toString(id) + " " + "O"
+				+ Boolean.toString(status));
+	}
+
+	void off() {
+		status = OFF;
+		hub.output("LED" + " " + Integer.toString(id) + " " + "O"
+				+ Boolean.toString(status));
+	}
+
+	void setStatus(boolean onoff) {
+		status = onoff;
+		hub.output("LED" + " " + Integer.toString(id) + " " + "O"
+				+ Boolean.toString(status));
+	}
+
+	boolean getStatus() {
+		return status;
+	}
+
+}
